9b6d175fb690bd0a4e4701df2769cb0c155416b1
Mattie Berry <102761430+MatthewKyleBerry@users.noreply.github.com>
2023-01-22 12:23:59 -0500

    Add files via upload
    Uploaded fixed version of code.py

--- code.py
+++ code.py
@@ -2,13 +2,24 @@ import time
 import usb_hid
 import board
 import digitalio
+import asyncio
+import rotaryio
+
 from adafruit_hid.mouse import Mouse
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
 #----------------Global-Variables----------------
 
-buttonDelay = 10
+buttonDelay = 10 #debounce iterations for buttons
+DialDelay = 10 #how much the dial needs to rotate to receive an input out of 360
+DialRatio = 120 #simulated detents
+
+VolRR = 0
+VolRL = 0
+VolLR = 0
+VolLL = 0
+
 
 #----------------Classes----------------
 
@@ -16,7 +27,7 @@ class Button:
     def __init__(self,GPIO):
         self.name = str(GPIO)
         self.port = digitalio.DigitalInOut(GPIO)
-        self.port.direction = digitalio.Direction.INPUT
+        self.port.pull = digitalio.Pull.DOWN
         self.state = self.port.value
         self.time = 0
     @property
@@ -34,27 +45,58 @@ class Button:
 
     def get_Time(self):
         return self.time
+    pass
         
     
     
 class Dial:
-    def __init__(self,GPIO1,GPIO2):
-        self.port1 = digitalio.DigitalInOut(GPIO1)
-        self.port2 = digitalio.DigitalInOut(GPIO1)
-        self.port1.direction = digitalio.Direction.INPUT
-        self.port2.direction = digitalio.Direction.INPUT
-        self.state1 = self.port1.value
-        self.state2 = self.port2.value
+    def __init__(self,GPIO):
+        self.port = digitalio.DigitalInOut(GPIO)
+        self.port.direction = digitalio.Direction.INPUT
+        self.port.pull = digitalio.Pull.DOWN
+
     @property
-    def value1(self):
-        return self.port1.value
-    @property
-    def value2(self):
-        return self.port2.value
-    
+    def value(self):
+        return self.port.value
+
+#----------------Button-setup----------------
+
+keb = Keyboard(usb_hid.devices)
+
+VolR = rotaryio.IncrementalEncoder(board.GP16,board.GP17)
+VolRLastPos = VolR.position
+VolL = rotaryio.IncrementalEncoder(board.GP18,board.GP19)
+VolLLastPos = VolL.position
+
+
+BTa = Button(board.GP15)
+BTb = Button(board.GP14)
+BTc = Button(board.GP13)
+BTd = Button(board.GP12)
+
+FXr = Button(board.GP11)
+FXl = Button(board.GP10)
+
+Strt = Button(board.GP9)
+
+mouse = Mouse(usb_hid.devices)
+x=0
+
 #----------------Functions----------------
+async def main():
+    sendBTa = asyncio.create_task(sendPress(BTa,Keycode.D))
+    sendBTb = asyncio.create_task(sendPress(BTb,Keycode.F))
+    sendBTc = asyncio.create_task(sendPress(BTc,Keycode.J))
+    sendBTd = asyncio.create_task(sendPress(BTd,Keycode.K))
     
-def sendPress(BTN,Key):
+    sendFXr = asyncio.create_task(sendPress(FXr,Keycode.C))
+    sendFXl =asyncio.create_task(sendPress(FXl,Keycode.M))
+    
+    sendStrt = asyncio.create_task(sendPress(Strt,Keycode.ONE))
+    
+    await asyncio.gather(sendBTa,sendBTb,sendBTc,sendBTd,sendFXr,sendFXl,sendStrt)
+    
+async def sendPress(BTN,Key):
     if BTN.state != BTN.value:
         BTN.state = BTN.value
         
@@ -67,57 +109,45 @@ def sendPress(BTN,Key):
             BTN.zero_Time()
     if (BTN.get_Time() <= buttonDelay):
         BTN.inc_Time()
-            
-            
-def sendDial(DAIL,keyR,keyL):
-    pass
-
-#----------------Button-setup----------------
-
-keb = Keyboard(usb_hid.devices)
-
-BTa = Button(board.GP15)
-print(BTa.state)
-
-BTb = Button(board.GP14)
-print(BTb.state)
-
-BTc = Button(board.GP13)
-print(BTc.state)
-
-BTd = Button(board.GP12)
-print(BTd.state)
-
-
-FXr = Button(board.GP11)
-print(FXr.state)
-
-FXl = Button(board.GP10)
-print(FXl.state)
-
-
-Strt = Button(board.GP9)
-print(Strt.state)
-
-
-mouse = Mouse(usb_hid.devices)
-x=0
 
 
 #----------------Runtime----------------
-
 while True:
-    sendPress(BTa,Keycode.D)
-    sendPress(BTb,Keycode.F)
-    sendPress(BTc,Keycode.J)
-    sendPress(BTd,Keycode.K)
+    VolRPos = VolR.position
+    VolLPos = VolL.position
     
-    sendPress(FXr,Keycode.C)
-    sendPress(FXl,Keycode.M)
+    if (VolRPos != VolRLastPos):
+        if VolRPos < VolRLastPos:
+            VolRR += 1
+            if VolRR > DialDelay:
+                VolRL = 0
+                keb.send(Keycode.P)
+                VolRR = DialDelay - 360/DialRatio
+                 
+        elif VolRPos > VolRLastPos:
+            VolRL += 1
+            if VolRL > DialDelay:
+                VolRR = 0
+                keb.send(Keycode.O)
+                VolRL = DialDelay - 360/DialRatio
+    VolRLastPos = VolRPos
     
-    sendPress(Strt,Keycode.K)
-        
-        #if BTa.value == True:
-        #    keb.press(Keycode.D)
-        #elif BTa.value == False:
-        #	 keb.release(Keycode.D)
+    if (VolLPos != VolLLastPos):
+        if VolLPos < VolLLastPos:
+            VolLR += 1
+            if VolLR > DialDelay:
+                VolLL = 0
+                keb.send(Keycode.E)
+                VolLR = DialDelay - 360/DialRatio
+                 
+        elif VolLPos > VolLLastPos:
+            VolLL += 1
+            if VolLL > DialDelay:
+                VolLR = 0
+                keb.send(Keycode.W)
+                VolLL = DialDelay - 360/DialRatio
+    VolLLastPos = VolLPos
+    
+    asyncio.run(main())
+    
+
